c1afd55d285a8f49d4487780fd09b56c3eb19200
tommyho973 <tommyho973@gmail.com>
2023-12-01 01:59:27 -0500

    Added firebase stuff

--- travel-app/src/app/app.module.ts
+++ travel-app/src/app/app.module.ts
@@ -3,6 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 @NgModule({
   declarations: [
@@ -10,9 +21,27 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"languagelogin-53aef","appId":"1:393887629758:web:adc0ece906532bdead39ef","storageBucket":"languagelogin-53aef.appspot.com","apiKey":"AIzaSyAA9BqT2s9cE8ExpBeXNhb7rfexg2i-rNw","authDomain":"languagelogin-53aef.firebaseapp.com","messagingSenderId":"393887629758","measurementId":"G-WQ7Z1QYJ8J"})),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    provideAppCheck(() => {
+      // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+      const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+      return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+    }),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage()),
+    provideRemoteConfig(() => getRemoteConfig())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,
+    UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ travel-app/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAA9BqT2s9cE8ExpBeXNhb7rfexg2i-rNw",
+        authDomain: "languagelogin-53aef.firebaseapp.com",
+        projectId: "languagelogin-53aef",
+        storageBucket: "languagelogin-53aef.appspot.com",
+        messagingSenderId: "393887629758",
+        appId: "1:393887629758:web:adc0ece906532bdead39ef",
+        measurementId: "G-WQ7Z1QYJ8J"
+    }
+  };
