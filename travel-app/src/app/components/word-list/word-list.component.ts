26f7b3a8005c6f7ec2d3050fc5fbcd8ed659fce2
tszwong <wongt@bu.edu>
2023-12-10 16:05:01 -0500

    testing word list component

--- travel-app/src/app/components/word-list/word-list.component.ts
+++ travel-app/src/app/components/word-list/word-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
 @Component({
@@ -6,21 +6,27 @@ import { AuthService } from '../../auth/auth.service';
   templateUrl: './word-list.component.html',
   styleUrls: ['./word-list.component.css']
 })
-export class WordListComponent {
+export class WordListComponent implements OnInit {
   userUid: string | null = null;
   flashcardList: any[] = [];
+
   constructor(private router: Router, public authService: AuthService) {
     authService.user$.subscribe((user) => {
       this.userUid = user ? user.uid : null;
     });
   }
+
+  ngOnInit(): void {
+    this.getFlashcardData();
+  }
+
   getFlashcardData(){
-    if(this.userUid!=null){
+    if(this.userUid){
       const url = `http://localhost:5000/api/flashcard-list?email=${this.userUid}`;
       fetch(url)
     .then(response => response.json())
     .then((data) => {
-      this.flashcardList = data.result; // Assuming the array is nested under 'result'
+      this.flashcardList = data.result;
       console.log(this.flashcardList);
     })
     .catch((error) =>
