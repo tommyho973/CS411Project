18c4090e86b1be9dc24c5a026a732586525bcd4c
tommyho973 <tommyho973@gmail.com>
2023-12-10 14:39:46 -0500

    Got form data

--- travel-app/src/app/create-your-own-flashcard/create-your-own-flashcard.component.spec.ts
+++ travel-app/src/app/create-your-own-flashcard/create-your-own-flashcard.component.spec.ts
@@ -1,10 +1,43 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { CreateYourOwnFlashcardComponent } from './create-your-own-flashcard.component';
 
-@NgModule({
-  imports: [
-    // ... other modules
-    FormsModule,
-  ],
-  // ...
-})
-export class YourModule { }
+describe('CreateYourOwnFlashcardComponent', () => {
+  let component: CreateYourOwnFlashcardComponent;
+  let fixture: ComponentFixture<CreateYourOwnFlashcardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FormsModule], // Import FormsModule for ngModel
+      declarations: [CreateYourOwnFlashcardComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateYourOwnFlashcardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should handle form submission', () => {
+    // Assuming you have a submitFlashcard method in your component
+    spyOn(component, 'submitFlashcard');
+
+    // Simulate user input
+    component.englishWord = 'Test Word';
+    component.englishTranslation = 'Test Translation';
+    component.translatedWord = 'Test Translated Word';
+    component.translatedDefinition = 'Test Translated Definition';
+
+    // Trigger the click event on the submit button
+    const submitButton = fixture.debugElement.nativeElement.querySelector('button');
+    submitButton.click();
+
+    // Check if the submitFlashcard method is called
+    expect(component.submitFlashcard).toHaveBeenCalled();
+  });
+});
--- travel-app/src/app/create-your-own-flashcard/create-your-own-flashcard.component.ts
+++ travel-app/src/app/create-your-own-flashcard/create-your-own-flashcard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-create-your-own-flashcard',
@@ -10,14 +12,27 @@ export class CreateYourOwnFlashcardComponent {
   englishTranslation: string = '';
   translatedWord: string = '';
   translatedDefinition: string = '';
+  userUid: string | null = null;
+
+  constructor(private router: Router,public authService: AuthService) {
+    authService.user$.subscribe((user) => {
+      this.userUid = user ? user.uid : null;
+    });
+  }
 
   submitFlashcard() {
     // Handle the submission logic here, e.g., send the data to the server
+    this.englishWord = (document.getElementById('englishWord') as HTMLInputElement)?.value;
+    this.englishTranslation = (document.getElementById('englishDefinition') as HTMLInputElement)?.value;
+    this.translatedWord = (document.getElementById('translatedWord') as HTMLInputElement)?.value;
+    this.translatedDefinition = (document.getElementById('translatedDefinition') as HTMLInputElement)?.value;
     console.log('Submitted Flashcard:', {
       englishWord: this.englishWord,
       englishTranslation: this.englishTranslation,
       translatedWord: this.translatedWord,
       translatedDefinition: this.translatedDefinition,
     });
+    
   }
+
 }
